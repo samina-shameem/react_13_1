d1bedb3a08305de82b49ae4ed92bb2634726942f
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-16 20:40:45 +0100

    first commit

---
+++ first_project/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  let x="samina";
+  let array=["use1","use2","use3","use4"];
+  return (
+    <div>
+      {
+        x
+      }
+      {
+        array.map((user)=>{return})
+      }
+    </div>
+  )
+}
+
+export default App
