eb01648033b5fe79cd73e5355337eaa3e10d1005
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-22 21:08:47 +0100

    import todocss and todo items css.

--- first_project/src/components/Todo.jsx
+++ first_project/src/components/Todo.jsx
@@ -1,5 +1,5 @@
 //import React from 'react'
-
+import './CSS/Todo.css'
 const Todo = () => {
   return (
     <div>
--- first_project/src/components/TodoItems.jsx
+++ first_project/src/components/TodoItems.jsx
@@ -1,4 +1,5 @@
 //import React from 'react'
+import './CSS/TodoItems.css'
 
 const TodoItems = () => {
   return (
