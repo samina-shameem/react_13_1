4fa2fe4bc128b51f625ebefa84f7ebe4642f8395
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-23 17:30:51 +0100

    styled input.

--- first_project/src/components/Todo.jsx
+++ first_project/src/components/Todo.jsx
@@ -1,12 +1,19 @@
 //import React from 'react'
+import { useRef, useState } from "react";
 import "./CSS/Todo.css";
 const Todo = () => {
+    const[todos,setTodos]=useState([]);
+    const inputRef=useRef(null);
+    const add=() =>{
+     setTodos([...todos,{}])
+    }
+
   return (
     <div className="todo">
       <div className="todo-header">TO-DO List</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add Your Task" className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input ref={inputRef}  type="text" placeholder="Add Your Task" className="todo-input" />
+        <div onClick={()=>{add()}}className="todo-add-btn">ADD</div>
       </div>
       <div className="todo-list">
         
