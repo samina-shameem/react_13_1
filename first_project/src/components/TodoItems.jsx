664535b597a5cbad3a9a06f0db5b550bda41005a
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-24 18:13:06 +0100

    no,display,text in todoitems.

--- first_project/src/components/TodoItems.jsx
+++ first_project/src/components/TodoItems.jsx
@@ -1,14 +1,18 @@
 //import React from 'react'
 import './CSS/TodoItems.css'
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png';
+import cross from './Assets/cross.png';
 
-const TodoItems = () => {
+const TodoItems = ({no,display,text}) => {
   return (
     <div className='todoitems'>
       <div className="todoitems-container">
-        <img src="" alt=""/>
-        <img src="" alt=""/>
+        <img src={not_tick} alt=""/>
+        <img src={tick} alt=""/>
+        <div className='todoitems-text'>{text}</div>
       </div>
-      
+      <img src={cross} alt=''/>
     </div>
   )
 }
