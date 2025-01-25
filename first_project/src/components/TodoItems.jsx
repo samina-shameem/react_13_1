c97b4a658448277a6d1cd8fad3ecbcda0304d52c
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-25 20:49:32 +0100

    add variable toggle and data

--- first_project/src/components/TodoItems.jsx
+++ first_project/src/components/TodoItems.jsx
@@ -5,11 +5,16 @@ import not_tick from './Assets/not_tick.png';
 import cross from './Assets/cross.png';
 
 const TodoItems = ({no,display,text}) => {
+   const toggle=() => {
+    let data= JSON.parse(localStorage.getItem("todos"));
+   }
+
   return (
     <div className='todoitems'>
       <div className="todoitems-container">
-        <img src={not_tick} alt=""/>
-        <img src={tick} alt=""/>
+        
+      {display===""?<img src={not_tick} alt=""/>:<img src={tick} alt=""/>}
+        
         <div className='todoitems-text'>{text}</div>
       </div>
       <img  className='todoitems-cross-icon' src={cross} alt=''/>
